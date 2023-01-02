2d116c46a958dcb2ea7eb1638da2d6fb78ed9cb4
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-02 21:35:18 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello dears');
+function a(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log('i :' + i);
+    for (let j = 1; j <= n; j++) {
+      console.log('j:' + j);
+    }
+  }
+}
+
+console.log(a(5));
