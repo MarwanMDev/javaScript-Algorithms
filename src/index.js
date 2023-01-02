a2aad81a27baee9c00e662d02843fcbe15bd6957
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-02 21:42:33 +0200

    bubble sort algorithm

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-function a(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log('i :' + i);
-    for (let j = 1; j <= n; j++) {
-      console.log('j:' + j);
+// function a(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log('i :' + i);
+//     for (let j = 1; j <= n; j++) {
+//       console.log('j:' + j);
+//     }
+//   }
+// }
+
+// console.log(a(5));
+
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
     }
-  }
+  } while (swapped);
 }
 
-console.log(a(5));
+const arr = [8, 20, -2, 4, -6];
+bubbleSort(arr);
+console.log(arr); // [-6, -2, 4, 8, 20]
