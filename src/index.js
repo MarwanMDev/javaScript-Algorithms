2ad8b7eee1b1a02480073f1a8af564f83cc6bf21
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-03 21:16:19 +0200

    merge Sort algorithm

--- src/index.js
+++ src/index.js
@@ -24,6 +24,30 @@ function bubbleSort(arr) {
   } while (swapped);
 }
 
+// const arr = [8, 20, -2, 4, -6];
+// bubbleSort(arr);
+// console.log(arr); // [-6, -2, 4, 8, 20]
+
+function mergeSort(arr) {
+  if (arr.length < 2) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+  const sortedArray = [];
+
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArray.push(leftArr.shift());
+    } else {
+      sortedArray.push(rightArr.shift());
+    }
+  }
+  return [...sortedArray, ...leftArr, ...rightArr];
+}
+
 const arr = [8, 20, -2, 4, -6];
-bubbleSort(arr);
-console.log(arr); // [-6, -2, 4, 8, 20]
+console.log(mergeSort(arr)); // [-6, -2, 4, 8, 20]
